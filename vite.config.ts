28bda7b7ea28199545adbf539aa857c62ff55bc9
Divij <ndivij2004@gmail.com>
2024-10-23 01:12:13 +0530

    second  commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,6 @@
-// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  test: {
-    environment: 'jsdom',
-    globals: true,
-    setupFiles: './src/setupTests.ts', // Optional: For setup files
-  },
 });
